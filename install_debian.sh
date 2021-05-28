4ddfefe0516a2f276d8ff005ab3817097713a890
Shankar Gomare <shankar.gomare@gmail.com>
2021-05-27 22:00:02 -0400

    Auto install script with user validation

--- install_debian.sh
+++ install_debian.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
+echo "Welcome to Shankar's featherOS install script!"
+echo "Please enter the username"
+read varuser
+id -u $varuser > /dev/null
+if [ $? -ne 0 ]; then
+	echo "The user is not valid"
+	exit -1
+fi
 apt -y install build-essential libx11-dev libxinerama-dev libxft-dev \
                git wget xinit xserver-xorg zsh x11-xserver-utils \
 	       fonts-font-awesome chromium curl
-chsh -s /bin/zsh
 git clone https://github.com/sgomare/featherOS.git /opt/featherOS/ 
 cd /opt/featherOS/dwm-6.2/
 make clean install
+make clean
 cd ../st-0.8.4
 make clean install
+make clean
 cd ../dmenu-5.0
 make clean install
+make clean
 cd ../dwmstatus
 make clean install
+make clean
 cd ../
-cp .zshrc ~/.zshrc
-cp .xinitrc ~/.xinitrc
-cp ./dircolors-solarized/dircolors.256dark ~/.dir_colors 
+chsh -s /bin/zsh $varuser
+cp .zshrc /home/$varuser/.zshrc
+cp .xinitrc /home/$varuser/.xinitrc
+cp ./dircolors-solarized/dircolors.256dark /home/$varuser/.dir_colors 
