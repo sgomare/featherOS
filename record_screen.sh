eb03bc59fa5fce8bd03173ae044b9cdd23632a46
Shankar Gomare <shankar.gomare@gmail.com>
2021-05-31 21:24:23 -0400

    Split recording script and webcam script

--- record_screen.sh
+++ record_screen.sh
@@ -1,2 +1,1 @@
 ffmpeg -hwaccel auto -f x11grab -video_size 1680x1050 -i :0.0 -f pulse -ac 2 -i default -vcodec libx264 -x264opts opencl -acodec pcm_s16le -b:a 384k out.mkv
-mpv --demuxer-lavf-format=video4linux2 --demuxer-lavf-o-set=input_format=mjpeg av://v4l2:/dev/video0
