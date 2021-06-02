e3866619d9ed741ec9b9173e141a9db199c33713
root <root@pop-os.localdomain>
2021-06-02 00:28:31 -0400

    Pop_OS install

---
+++ install_popos.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Welcome to Shankar's featherOS install script!"
+echo "Please enter the username"
+read varuser
+id -u $varuser > /dev/null
+if [ $? -ne 0 ]; then
+	echo "The user is not valid"
+	exit -1
+fi
+apt -y purge pop-desktop gnome-shell
+apt autoremove
+apt -y install build-essential libx11-dev libxinerama-dev libxft-dev \
+               git wget xinit xserver-xorg zsh x11-xserver-utils \
+	       fonts-font-awesome chromium curl
+cd /opt/featherOS/dwm-6.2/
+make clean install
+make clean
+cd ../st-0.8.4
+make clean install
+make clean
+cd ../dmenu-5.0
+make clean install
+make clean
+cd ../dwmstatus
+make clean install
+make clean
+cd ../
+chsh -s /bin/zsh $varuser
+cp .zshrc /home/$varuser/.zshrc
+cp .xinitrc /home/$varuser/.xinitrc
+cp ./dircolors-solarized/dircolors.256dark /home/$varuser/.dir_colors 
